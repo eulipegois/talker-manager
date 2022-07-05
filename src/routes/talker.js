a40d5c76548267a6baa1c8e86d342eb3939440c5
Luiz Felipe Gois <luizz3.14@hotmail.com>
2022-07-05 17:21:40 -0500

    refactor: code structure improvement endpoint GET/talker/:id

--- src/routes/talker.js
+++ src/routes/talker.js
@@ -13,4 +13,17 @@ routerTalker.get('/', async (_req, res) => {
   res.status(200).json(talkers);
 });
 
+routerTalker.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await getTalkers();
+
+  const talker = talkers.find((index) => index.id === Number(id));
+
+  if (!talker || talker === undefined) {
+    res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  res.status(200).json(talker);
+});
+
 module.exports = routerTalker;
