5f0a1882dc4453aaee33d7b380ffea0200333497
Luiz Felipe Gois <luizz3.14@hotmail.com>
2022-07-05 16:05:10 -0500

    feat: create endpoint GET /talker/:id

--- src/routes/talker.js
+++ src/routes/talker.js
@@ -20,4 +20,19 @@ routerTalker.get('/', async (req, res) => {
   res.status(200).json(data);
 });
 
+// Busca Talker pelo seu ID
+routerTalker.get('/:id', async (req, res) => {
+  // busca o id
+  const id = Number(req.params.id);
+  // Ler o arquivo JSON e salva as informações
+  const data = JSON.parse(await fs.readFile(PERSONS_JSON_PATH, 'utf-8'));
+  // Mapeia as informações em (data) e buscar se há um Talker com o mesmo ID do parametro na requisição, se não houver ele retorna -1.
+  const result = data.findIndex((talker) => talker.id === id);
+
+  // Caso retorne -1 não temos o id fornecido no parametro
+  if (result < 0) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  res.json(data[result]);
+});
+
 module.exports = routerTalker;
